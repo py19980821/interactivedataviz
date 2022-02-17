6abe45995326484a51a24bd65d9ee8d573f5a6d0
Yue Pan <you@example.com>
2022-02-16 20:24:38 -0500

    Class

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,15 +1,16 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+const width = window.innerWidth * 0.08;
+const height = window.innerHeight / 3;
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
-    
+    const xScale = ;
+    const yScale = ;
 
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
