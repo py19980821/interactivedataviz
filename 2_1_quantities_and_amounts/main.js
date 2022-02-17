20de3630f5c14dfb172e4585238e3696566cbbe8
Yue Pan <you@example.com>
2022-02-16 20:30:11 -0500

    class

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -9,9 +9,12 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
-    const xScale = ;
-    const yScale = ;
-
+    const xScale = d3.scaleBand()
+    .domain(data.map(d => d.activity))
+    .range([0,width])
+    const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.count)])
+    .range([height, 0])
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
 
