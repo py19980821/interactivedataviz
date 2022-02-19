0cc99df4e87bd945e09620523acce31fc36ff928
Yue Pan <you@example.com>
2022-02-19 16:52:51 -0500

    friend name

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,17 @@
 console.log('hello world');
+
+const label = document.getElementById("friend-label")
+const input = document.getElementById("friend-input")
+const button = document.getElementById("friend-submit")
+
+let friendName;
+let friendCount = 0;
+
+function friendNameUpdate() {
+    friendName = input.value
+    console.log(friendName)
+    friendCount = friendCount + 1;
+    label.innerText = friendName + " is your friend! You have total " + friendCount + " friend(s)."
+    button.innerText = "Tell me another friends of yours!"
+    input.value = ""
+}
