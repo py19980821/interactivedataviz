5fab6ac818e85242ad2087c99f6724fb52df7890
Yue Pan <you@example.com>
2022-02-23 20:39:32 -0500

    In class

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,6 +1,6 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.08;
-const height = window.innerHeight / 3;
+const width = window.innerWidth * 0.8;
+const height = window.innerHeight * 0.8;
 
 /* LOAD DATA */
 d3.csv('../data/squirrelActivities.csv', d3.autoType)
@@ -17,5 +17,18 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
     .range([height, 0])
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
+    const svg = d3.select("#barchart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
+    // SELECT - JOIN - DRAW
+    svg.selectAll("rect")
+       .data(data)
+       .join("rect")
+       .attr("width", xScale.bandwidth())
+       .attr("height", d => height - yScale(d.count))
+       .attr("x", d => xScale(d.activity))
+       .attr("y", d => yScale(d.count))
 
   })
--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,34 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.8,
+      height = window.innerHeight * 0.8,
+      margin = {top: 10, botton: 10, left: 10, right: 10};
 
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
+d3.json("../data/environmentRatings.json", d3.autoType)
   .then(data => {
     console.log(data)
 
     /* SCALES */
-    
+    const xScale = d3.scaleLinear()
+       .domain([0, d3.max(data, d => d.envScore2020)])
+       .range([margin.left, width - margin.right])
+
+    const yScale = d3.scaleLinear()
+       .domain([0, d3.max(data, d => d.ideologyScore2020)])
+       .range([height - margin.botton, margin.top])
+
     /* HTML ELEMENTS */
+    const svg = d3.select("#container")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
     
+    //create circles via SELECT-DATA-JOIN
+    svg.selectAll("circle")
+       .data(data, d => d.BioID)
+       .join("circle")
+       .attr("cx", d => xScale(d.envScore2020))
+       .attr("cy", d => yScale(d.ideologyScore2020))
+       .attr("r", 5)
+
   });
