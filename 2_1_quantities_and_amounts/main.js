087c168c8c484733e3fdc0b4ed704170b114dafd
Yue Pan <you@example.com>
2022-03-02 16:50:35 -0500

    horizontal

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,17 +1,45 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+const width = window.innerWidth *.8 ;
+const height = window.innerHeight *.8;
+
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
+.then(data => {
+  console.log("data", data)
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
+  /* SCALES */
+  // xscale - categorical, activity
+  const yScale = d3.scaleBand()
+                   .domain(data.map(d=> d.activity))
+                   .range([0, height])
+                   .paddingInner(.1);
+                  
+                    // visual variable
     
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+    // yscale - linear,count
+  const xScale = d3.scaleLinear()
+                   .domain([0, 1500])
+                   .range([0, width])
+    
+  /* HTML ELEMENTS */
+  // svg
+  const svg = d3.select("#container")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                
+
+  // bars
+  svg.selectAll("rect")
+     .data(data)
+     .join("rect")
+     .attr("class", "bar")
+     .attr("height", yScale.bandwidth())
+     .attr("width", d=> xScale(d.count))
+     .attr("x", xScale(0))
+     .attr("y", d=> yScale(d.activity))
+     .attr("fill", "#69b3a2")
 
   })
