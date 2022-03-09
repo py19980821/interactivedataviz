18ca661e927cd77084db75ef9f6e20c97ba5cb1a
Yue Pan <you@example.com>
2022-03-09 15:15:48 -0500

    class

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,5 +1,5 @@
  /* CONSTANTS AND GLOBALS */
-const width = window.inerWidth * .7,
+const width = window.innerWidth * .7,
       height = window.innerWidth * .7,
       margin = 30;
 
@@ -14,13 +14,33 @@ d3.csv('/Users/panyue/Documents/学习/Master/Semester/SPRING2022/DATA73200/inte
   console.log('data :>> ', data);
 
   // SCALES
-
+  const xScale = d3.scaleTime()
+     .domain(d3.extent(data, d=> d.year))
+     .range([margin, width-margin])
+  
+  const yScale = d3.scaleLinear()
+     .domain(d3.extent(data, d=> d.population))
+     .range([height-margin,margin])   
   // CREATE SVG ELEMENT
+
+  const svg = d3.select("#container")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
 
   // BUILD AND CALL AXES
 
   // LINE GENERATOR FUNCTION
-
+  const LineGen = d3.line()
+          .x(d => xScale(d.year))
+          .y(d => yScale(d.population))
   // DRAW LINE
+  svg.selectAll("path.line")
+     .data([data])
+     .join("path.line")
+     .attr("class", "line")
+     .attr("stroke", "blue")
+     .attr("fill", "none")
+     .attr("d", d=>LineGen(d))
 
 });
